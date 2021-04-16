c9595edc259a0101a1bb48d63e63d2dd8a41f2c7
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2021-04-16 15:03:10 +0200

    Try nav witout env-vars

--- components/Nav.js
+++ components/Nav.js
@@ -6,17 +6,33 @@ const Nav = () => {
 // export default function Nav(props) {
   const PATH = process.env.NEXT_PUBLIC_BASE_PATH
 
+  // return (
+  //   <nav className={navStyles.nav}>
+  //     <ul>
+  //       <li>
+  //         <Link href={{ pathname: `${PATH}`}}><a>Home</a></Link>
+  //       </li>
+  //       <li>
+  //         <Link href={{ pathname: `${PATH}/login`}}><a>Login</a></Link>
+  //       </li>
+  //       <li>
+  //         <Link href={{ pathname: `${PATH}/register`}}><a>Register</a></Link>
+  //       </li>
+  //     </ul>
+  //   </nav>
+  // )
+
   return (
     <nav className={navStyles.nav}>
       <ul>
         <li>
-          <Link href={{ pathname: `${PATH}`}}><a>Home</a></Link>
+          <Link href='/'><a>Home</a></Link>
         </li>
         <li>
-          <Link href={{ pathname: `${PATH}/login`}}><a>Login</a></Link>
+          <Link href='/login'><a>Login</a></Link>
         </li>
         <li>
-          <Link href={{ pathname: `${PATH}/register`}}><a>Register</a></Link>
+          <Link href='/register'><a>Register</a></Link>
         </li>
       </ul>
     </nav>
