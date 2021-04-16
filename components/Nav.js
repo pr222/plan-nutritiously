58a8acc9f38988d4704f1fce74c0fdeaa3cb5db3
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2021-04-16 10:48:19 +0200

    Configure paths

--- components/Nav.js
+++ components/Nav.js
@@ -1,22 +1,30 @@
 import Link from 'next/link'
 import navStyles from '../styles/Nav.module.css'
 
+// const PATH = process.env.BASE_PATH
 const Nav = () => {
+// export default function Nav(props) {
+  const PATH = process.env.NEXT_PUBLIC_BASE_PATH
+
   return (
     <nav className={navStyles.nav}>
       <ul>
         <li>
-          <Link href="/"><a>Home</a></Link>
+          <Link href={{ pathname: `${PATH}`}}><a>Home</a></Link>
         </li>
         <li>
-          <Link href="/login"><a>Login</a></Link>
+          <Link href={{ pathname: `${PATH}/login`}}><a>Login</a></Link>
         </li>
         <li>
-          <Link href="/register"><a>Register</a></Link>
+          <Link href={{ pathname: `${PATH}/register`}}><a>Register</a></Link>
         </li>
       </ul>
     </nav>
   )
+}
+
+export async function getStaticProps() {
+  return {    props: {}    }
 }
 
 export default Nav
