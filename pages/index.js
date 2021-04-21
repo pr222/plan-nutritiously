d9ffa9f2379572638a5f2cd6539bf1a67fc4f660
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2021-04-21 12:56:59 +0200

    Add connection to database

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
+import PropTypes from 'prop-types';
+import connectToDatabase from '../middleware/mongodb';
 
-export default function Index() {
+export default function Home({ isConnected }) {
   return (
     <>
       <Head>
@@ -8,6 +10,26 @@ export default function Index() {
       </Head>
       <h1>Welcome!</h1>
       <p>Plan your groceries nutritiously</p>
+
+      {isConnected ? (
+        console.log('You are connected to MongoDB')
+      ) : (
+        console.log('You are NOT connected to MongoDB')
+      )}
     </>
   );
 }
+
+export async function getServerSideProps() {
+  const { client } = await connectToDatabase();
+
+  const isConnected = await client.isConnected();
+
+  return {
+    props: { isConnected },
+  };
+}
+
+Home.propTypes = {
+  isConnected: PropTypes.bool.isRequired,
+};
